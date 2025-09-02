a31f70eb33ef35a0e3e52e41a5943278d125a3cf
Vukasin <vukasin.reality@gmail.com>
2025-09-02 17:45:58 +0200

    u dogovoru sa profesorkom-ispravka

--- chat-app/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-		remotePatterns: [{ hostname: "res.cloudinary.com" }],
-	}
-};
-
-export default nextConfig;
